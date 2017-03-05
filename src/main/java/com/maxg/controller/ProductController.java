94f3f64bfafeaa67131ac5217449e2defa6fd6f1
maxiaogang <465130623@qq.com>
2017-03-05 10:28:45 +0800

    init环境搭建

---
+++ src/main/java/com/maxg/controller/ProductController.java
@@ -0,0 +1,10 @@
+package com.maxg.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by 46513 on 2017/3/5.
+ */
+@Controller
+public class ProductController {
+}
---
+++ src/main/java/com/maxg/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.maxg.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by 46513 on 2017/3/5.
+ */
+@Service
+public class ProductService {
+}
