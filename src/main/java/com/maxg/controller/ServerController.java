92ac607fc0680104b1a063a58fcc86654a81721c
maxiaogang <465130623@qq.com>
2017-03-05 10:50:33 +0800

    添加http四中测试服务

--- src/main/java/com/maxg/controller/ServerController.java
+++ src/main/java/com/maxg/controller/ServerController.java
@@ -1,10 +1,64 @@
 package com.maxg.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by 46513 on 2017/3/5.
  */
-@Controller
+@RestController
+@RequestMapping("/server")
 public class ServerController {
+
+    /**
+     * get 服务
+     * @return
+     */
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public Map<String, Object> getServer(){
+        Map<String, Object> resMap = new HashMap<>();
+        resMap.put("code", 0);
+        resMap.put("method", "get");
+        return resMap;
+    }
+
+    /**
+     * delete 服务
+     * @return
+     */
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public Map<String, Object> deleteServer(){
+        Map<String, Object> resMap = new HashMap<>();
+        resMap.put("code", 0);
+        resMap.put("method", "delete");
+        return resMap;
+    }
+
+    /**
+     * post服务
+     * @return
+     */
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    public Map<String, Object> postServer(){
+        Map<String, Object> resMap = new HashMap<>();
+        resMap.put("code", 0);
+        resMap.put("method", "post");
+        return resMap;
+    }
+
+    /**
+     * put服务
+     * @return
+     */
+    @RequestMapping(value = "/put", method = RequestMethod.PUT)
+    public Map<String, Object> putServer(){
+        Map<String, Object> resMap = new HashMap<>();
+        resMap.put("code", 0);
+        resMap.put("method", "put");
+        return resMap;
+    }
 }
